a4624cb640fb54edcd814b3897223c01b4dada95
Martin Utzon <famutzon@outlook.dk>
2018-11-11 10:59:45 +0100

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Martin Utzon on 11/11/2018.
+//  Copyright © 2018 Martin Utzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
