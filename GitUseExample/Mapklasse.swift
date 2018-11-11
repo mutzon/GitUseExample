41d9c841a68ebfd5fe3177d07a956248ac444132
Martin Utzon <famutzon@outlook.dk>
2018-11-11 11:29:54 +0100

    map_feature branch og lavet en ny klasse MapKlasse

---
+++ GitUseExample/Mapklasse.swift
@@ -0,0 +1,13 @@
+//
+//  Mapklasse.swift
+//  GitUseExample
+//
+//  Created by Martin Utzon on 11/11/2018.
+//  Copyright © 2018 Martin Utzon. All rights reserved.
+//
+
+import UIKit
+
+class Mapklasse: NSObject {
+
+}
